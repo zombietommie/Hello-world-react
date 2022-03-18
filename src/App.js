1f0120dc5f7bd9434c72c03cc7b2dfb200d5b0fa
Tommy <36934180+zombietommie@users.noreply.github.com>
2022-03-18 10:47:23 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import { Button } from "@mui/material";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Hello World, this is React</h2>
+      <Button variant="contained">Login</Button>
+    </div>
+  );
+}
